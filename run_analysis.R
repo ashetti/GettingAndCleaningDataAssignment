84492ba31d8a830ea93d517a9dad7af9fe16d588
Abhijit Shetti <ashetti@gmail.com>
2014-04-27 15:19:57 -0700

    Added label values

--- run_analysis.R
+++ run_analysis.R
@@ -9,6 +9,7 @@ urltrainy <- "./UCI HAR Dataset/train/y_train.txt"
 urltestsub <- "./UCI HAR Dataset/test/subject_test.txt"
 urltrainsub <- "./UCI HAR Dataset/train/subject_train.txt"
 urlfeatures <- "./UCI HAR Dataset/features.txt"
+urlactivityLabels <- "./UCI HAR Dataset/activity_labels.txt"
 ## read the test and train feature sets and labels
 testx <-read.delim(urltestx,header=FALSE,sep="")
 testy <-read.delim(urltesty,header=FALSE,sep="")
@@ -17,12 +18,20 @@ trainy <- read.delim(urltrainy,header=FALSE,sep="")
 testsub <- read.delim(urltestsub,header=FALSE,sep="")
 trainsub <- read.delim(urltrainsub,header=FALSE,sep="")
 features <- read.delim(urlfeatures,header=FALSE,sep="",stringsAsFactors=FALSE)
-
+activityLabels<- read.delim(urlactivityLabels,header=FALSE,sep="",stringsAsFactors=FALSE)
 ##Get logical vectors to identify the means and std features 
 meansL <- grepl("mean",features$V2)
 stdL <- grepl("std",features$V2)
+
 ##Merge the training and test data sets
 featureValues <- rbind(testx,trainx)
-labels<-rbind(testy,trainy)
+labelValues<-rbind(testy,trainy)
+
 colNames <- c(features$V2[meansL | stdL])
 
+##Assign names to the columns
+names(labelValues) <- "ActivityLabel"
+names(featureValues) <- features$V2
+
+##Now add the activity labels to the individual observations
+mergedDataSet <- cbind(labelValues,featureValues)

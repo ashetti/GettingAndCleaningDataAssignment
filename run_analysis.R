7eec9e669e999891b1f7ed662fc123d0002a70ec
Abhijit Shetti <ashetti@gmail.com>
2014-06-22 15:05:48 -0700

    Base version

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,5 @@
+### Peer assessed assignment to create tidy data
+
 ##run_analysis.R
 ##In this program, we will read data from the UCI HAR data set. This program assumes the data files are available 
 ##in the same folder structure as the original data set.
@@ -20,35 +22,50 @@ testsub <- read.delim(urltestsub,header=FALSE,sep="")
 trainsub <- read.delim(urltrainsub,header=FALSE,sep="")
 features <- read.delim(urlfeatures,header=FALSE,sep="",stringsAsFactors=FALSE)
 activityLabels<- read.delim(urlactivityLabels,header=FALSE,sep="",stringsAsFactors=FALSE)
-##Get logical vectors to identify the means and std features 
-meansL <- grepl("mean",features$V2)
-stdL <- grepl("std",features$V2)
+
 
 ##Merge the training and test data sets
 featureValues <- rbind(testx,trainx)
 labelValues<-rbind(testy,trainy)
 subjectIds <- rbind(testsub,trainsub)
 
-colNames <- c(features$V2[meansL | stdL])
-
 ##Assign names to the columns
 names(labelValues) <- "ActivityLabel"
 names(featureValues) <- features$V2
 names(activityLabels)<-c("ActivityLabel","ActivityShortDescription")
 names(subjectIds)<-"SubjectID"
+
 ##Join activityLabels to labelValues to get short description
 labelValues <- join(x=labelValues,y=activityLabels,by="ActivityLabel")
 
-##Select columns with mean and std
+##Identify the column variables having mean and standard deviation values
+##find the features which have mean and std in their variable name
+meansL <- grepl("mean",features$V2)
+stdL <- grepl("std",features$V2)
+colNames <- c(features$V2[meansL | stdL])
+##Extract the columns with mean and std
 selectedFeatureValues <- featureValues[, which(names(featureValues) %in% colNames)]
 
-##Now add the subject Ids and activity labels to the individual observations
+##Cleanup column names
+##Remove non alpha numeric characters
+varNames <- names(selectedFeatureValues)
+varNames <- gsub("[^[:alnum:]///' ]", "", varNames)
+##Expand abbreviations and capitalize words
+varNames <- gsub("Acc", "Acceleration", varNames)
+varNames <- gsub("Mag", "Magnitude", varNames)
+varNames <- gsub("mean", "Mean", varNames)
+varNames <- gsub("std", "StdDev", varNames)
+##Replace the column names with the cleaned columnnames
+names(selectedFeatureValues)<-varNames
+
+
+##Now combine the subjectId's, and labels with the selected feature values
 mergedDataSet <- cbind(subjectIds,labelValues,selectedFeatureValues)
 
-##Now create the tidy data set
-fby1 <- factor(mergedDataSet$SubjectID)
-fby2 <- factor(mergedDataSet$ActivityShortDescription)
-tidyData <- aggregate(x=mergedDataSet,by=list(fby1,fby2),FUN=mean)
+##Load the plyr package
+library("plyr")
+##Now create the tidy data set by taking column wise mean for each observation of subject and Activity type
+tidyData <- ddply(mergedDataSet,.(SubjectID,ActivityLabel,ActivityShortDescription),colwise(mean))
 
-##Now write to a file TidySamsungData.txt
-dump(list(tidyData),file="TidySamsungData.txt")
+##Dump into an output file
+write.table(tidyData,file="tidyDataOutput.txt",append=FALSE,sep=" ",eol="\n",row.names=FALSE)
